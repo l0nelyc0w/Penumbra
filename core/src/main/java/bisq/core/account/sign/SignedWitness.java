sqrrm/bitsquare-1

Add domain layer for signed account age witnesses (credits ManfredKarrer and oscarguindzberg)

core/src/main/java/bisq/core/account/sign/SignedWitness.java
@@ -1,0 +1,170 @@
+/*
+ * This file is part of Bisq.
+ *
+ * bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.core.account.sign;
+
+import bisq.network.p2p.storage.P2PDataStorage;
+import bisq.network.p2p.storage.payload.CapabilityRequiringPayload;
+import bisq.network.p2p.storage.payload.DateTolerantPayload;
+import bisq.network.p2p.storage.payload.LazyProcessedPayload;
+import bisq.network.p2p.storage.payload.PersistableNetworkPayload;
+
+import bisq.common.app.Capabilities;
+import bisq.common.app.Capability;
+import bisq.common.crypto.Hash;
+import bisq.common.proto.persistable.PersistableEnvelope;
+import bisq.common.util.Utilities;
+
+import io.bisq.generated.protobuffer.PB;
+
+import com.google.protobuf.ByteString;
+
+import org.bitcoinj.core.Coin;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+
+// Supports signatures made from EC key (arbitrators) and signature created with DSA key.
+@Slf4j
+@Value
+public class SignedWitness implements LazyProcessedPayload, PersistableNetworkPayload, PersistableEnvelope,
+        DateTolerantPayload, CapabilityRequiringPayload {
+    private static final long TOLERANCE = TimeUnit.DAYS.toMillis(1);
+
+    private final boolean signedByArbitrator;
+    private final byte[] witnessHash;
+    private final byte[] signature;
+    private final byte[] signerPubKey;
+    private final byte[] witnessOwnerPubKey;
+    private final long date;
+    private final long tradeAmount;
+
+    transient private final byte[] hash;
+
+    public SignedWitness(boolean signedByArbitrator,
+                         byte[] witnessHash,
+                         byte[] signature,
+                         byte[] signerPubKey,
+                         byte[] witnessOwnerPubKey,
+                         long date,
+                         long tradeAmount) {
+        this.signedByArbitrator = signedByArbitrator;
+        this.witnessHash = witnessHash;
+        this.signature = signature;
+        this.signerPubKey = signerPubKey;
+        this.witnessOwnerPubKey = witnessOwnerPubKey;
+        this.date = date;
+        this.tradeAmount = tradeAmount;
+
+        // The hash is only using the data which do not change in repeated trades between same users (no date or amount).
+        // We only want to store the first and oldest one and will ignore others. That will help also to protect privacy
+        // so that the total number of trades is not revealed. We use putIfAbsent when we store the data so first
+        // object will win. We consider one signed trade with one peer enough and do not consider repeated trades with
+        // same peer to add more security as if that one would be colluding it would be not detected anyway. The total
+        // number of signed trades with different peers is still available and can be considered more valuable data for
+        // security.
+        byte[] data = Utilities.concatenateByteArrays(witnessHash, signature);
+        data = Utilities.concatenateByteArrays(data, signerPubKey);
+        hash = Hash.getSha256Ripemd160hash(data);
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // PROTO BUFFER
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public PB.PersistableNetworkPayload toProtoMessage() {
+        final PB.SignedWitness.Builder builder = PB.SignedWitness.newBuilder()
+                .setSignedByArbitrator(signedByArbitrator)
+                .setWitnessHash(ByteString.copyFrom(witnessHash))
+                .setSignature(ByteString.copyFrom(signature))
+                .setSignerPubKey(ByteString.copyFrom(signerPubKey))
+                .setWitnessOwnerPubKey(ByteString.copyFrom(witnessOwnerPubKey))
+                .setDate(date)
+                .setTradeAmount(tradeAmount);
+        return PB.PersistableNetworkPayload.newBuilder().setSignedWitness(builder).build();
+    }
+
+    public PB.SignedWitness toProtoSignedWitness() {
+        return toProtoMessage().getSignedWitness();
+    }
+
+    public static SignedWitness fromProto(PB.SignedWitness proto) {
+        return new SignedWitness(proto.getSignedByArbitrator(),
+                proto.getWitnessHash().toByteArray(),
+                proto.getSignature().toByteArray(),
+                proto.getSignerPubKey().toByteArray(),
+                proto.getWitnessOwnerPubKey().toByteArray(),
+                proto.getDate(),
+                proto.getTradeAmount());
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // API
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public boolean isDateInTolerance() {
+        // We don't allow older or newer then 1 day.
+        // Preventing forward dating is also important to protect against a sophisticated attack
+        return Math.abs(new Date().getTime() - date) <= TOLERANCE;
+    }
+
+    @Override
+    public boolean verifyHashSize() {
+        return hash.length == 20;
+    }
+
+    // Pre 1.0.1 version don't know the new message type and throw an error which leads to disconnecting the peer.
+    @Override
+    public Capabilities getRequiredCapabilities() {
+        return new Capabilities(Capability.SIGNED_ACCOUNT_AGE_WITNESS);
+    }
+
+    @Override
+    public byte[] getHash() {
+        return hash;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // Getters
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    public P2PDataStorage.ByteArray getHashAsByteArray() {
+        return new P2PDataStorage.ByteArray(hash);
+    }
+
+    @Override
+    public String toString() {
+        return "SignedWitness{" +
+                ",\n     signedByArbitrator=" + signedByArbitrator +
+                ",\n     witnessHash=" + Utilities.bytesAsHexString(witnessHash) +
+                ",\n     signature=" + Utilities.bytesAsHexString(signature) +
+                ",\n     signerPubKey=" + Utilities.bytesAsHexString(signerPubKey) +
+                ",\n     witnessOwnerPubKey=" + Utilities.bytesAsHexString(witnessOwnerPubKey) +
+                ",\n     date=" + date +
+                ",\n     tradeAmount=" + Coin.valueOf(tradeAmount).toFriendlyString() +
+                ",\n     hash=" + Utilities.bytesAsHexString(hash) +
+                "\n}";
+    }
+}

core/src/main/java/bisq/core/account/sign/SignedWitnessStore.java
@@ -15,7 +15,8 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.core.payment;
+package bisq.core.account.sign;
+
 
 import bisq.network.p2p.storage.P2PDataStorage;
 import bisq.network.p2p.storage.payload.PersistableNetworkPayload;
@@ -41,11 +42,11 @@ import lombok.extern.slf4j.Slf4j;
  * definition and provide a hashMap for the domain access.
  */
 @Slf4j
-public class AccountAgeWitnessStore implements PersistableEnvelope {
+public class SignedWitnessStore implements PersistableEnvelope {
     @Getter
     private Map<P2PDataStorage.ByteArray, PersistableNetworkPayload> map = new ConcurrentHashMap<>();
 
-    AccountAgeWitnessStore() {
+    SignedWitnessStore() {
     }
 
 
@@ -53,28 +54,28 @@ public class AccountAgeWitnessStore implements PersistableEnvelope {
     // PROTO BUFFER
     ///////////////////////////////////////////////////////////////////////////////////////////
 
-    private AccountAgeWitnessStore(List<AccountAgeWitness> list) {
+    private SignedWitnessStore(List<SignedWitness> list) {
         list.forEach(item -> map.put(new P2PDataStorage.ByteArray(item.getHash()), item));
     }
 
     public Message toProtoMessage() {
         return PB.PersistableEnvelope.newBuilder()
-                .setAccountAgeWitnessStore(getBuilder())
+                .setSignedWitnessStore(getBuilder())
                 .build();
     }
 
-    private PB.AccountAgeWitnessStore.Builder getBuilder() {
-        final List<PB.AccountAgeWitness> protoList = map.values().stream()
-                .map(payload -> (AccountAgeWitness) payload)
-                .map(AccountAgeWitness::toProtoAccountAgeWitness)
+    private PB.SignedWitnessStore.Builder getBuilder() {
+        final List<PB.SignedWitness> protoList = map.values().stream()
+                .map(payload -> (SignedWitness) payload)
+                .map(SignedWitness::toProtoSignedWitness)
                 .collect(Collectors.toList());
-        return PB.AccountAgeWitnessStore.newBuilder().addAllItems(protoList);
+        return PB.SignedWitnessStore.newBuilder().addAllItems(protoList);
     }
 
-    public static PersistableEnvelope fromProto(PB.AccountAgeWitnessStore proto) {
-        List<AccountAgeWitness> list = proto.getItemsList().stream()
-                .map(AccountAgeWitness::fromProto).collect(Collectors.toList());
-        return new AccountAgeWitnessStore(list);
+    public static PersistableEnvelope fromProto(PB.SignedWitnessStore proto) {
+        List<SignedWitness> list = proto.getItemsList().stream()
+                .map(SignedWitness::fromProto).collect(Collectors.toList());
+        return new SignedWitnessStore(list);
     }
 
     public boolean containsKey(P2PDataStorage.ByteArray hash) {

core/src/main/java/bisq/core/account/witness/AccountAgeWitnessStore.java
@@ -1,0 +1,83 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.core.account.witness;
+
+import bisq.network.p2p.storage.P2PDataStorage;
+import bisq.network.p2p.storage.payload.PersistableNetworkPayload;
+
+import bisq.common.proto.persistable.PersistableEnvelope;
+
+import io.bisq.generated.protobuffer.PB;
+
+import com.google.protobuf.Message;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+
+/**
+ * We store only the payload in the PB file to save disc space. The hash of the payload can be created anyway and
+ * is only used as key in the map. So we have a hybrid data structure which is represented as list in the protobuffer
+ * definition and provide a hashMap for the domain access.
+ */
+@Slf4j
+public class AccountAgeWitnessStore implements PersistableEnvelope {
+    @Getter
+    private Map<P2PDataStorage.ByteArray, PersistableNetworkPayload> map = new ConcurrentHashMap<>();
+
+    AccountAgeWitnessStore() {
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // PROTO BUFFER
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    private AccountAgeWitnessStore(List<AccountAgeWitness> list) {
+        list.forEach(item -> map.put(new P2PDataStorage.ByteArray(item.getHash()), item));
+    }
+
+    public Message toProtoMessage() {
+        return PB.PersistableEnvelope.newBuilder()
+                .setAccountAgeWitnessStore(getBuilder())
+                .build();
+    }
+
+    private PB.AccountAgeWitnessStore.Builder getBuilder() {
+        final List<PB.AccountAgeWitness> protoList = map.values().stream()
+                .map(payload -> (AccountAgeWitness) payload)
+                .map(AccountAgeWitness::toProtoAccountAgeWitness)
+                .collect(Collectors.toList());
+        return PB.AccountAgeWitnessStore.newBuilder().addAllItems(protoList);
+    }
+
+    public static PersistableEnvelope fromProto(PB.AccountAgeWitnessStore proto) {
+        List<AccountAgeWitness> list = proto.getItemsList().stream()
+                .map(AccountAgeWitness::fromProto).collect(Collectors.toList());
+        return new AccountAgeWitnessStore(list);
+    }
+
+    public boolean containsKey(P2PDataStorage.ByteArray hash) {
+        return map.containsKey(hash);
+    }
+}

l0nelyc0w/Penumbra

core/src/main/java/bisq/core/account/sign/SignedWitness.java
@@ -1,0 +1,169 @@
+/*
+ * This file is part of Bisq.
+ *
+ * bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.core.account.sign;
+
+import bisq.network.p2p.storage.P2PDataStorage;
+import bisq.network.p2p.storage.payload.CapabilityRequiringPayload;
+import bisq.network.p2p.storage.payload.DateTolerantPayload;
+import bisq.network.p2p.storage.payload.LazyProcessedPayload;
+import bisq.network.p2p.storage.payload.PersistableNetworkPayload;
+
+import bisq.common.app.Capabilities;
+import bisq.common.app.Capability;
+import bisq.common.crypto.Hash;
+import bisq.common.proto.persistable.PersistableEnvelope;
+import bisq.common.util.Utilities;
+
+
+import com.google.protobuf.ByteString;
+
+import org.bitcoinj.core.Coin;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+
+// Supports signatures made from EC key (arbitrators) and signature created with DSA key.
+@Slf4j
+@Value
+public class SignedWitness implements LazyProcessedPayload, PersistableNetworkPayload, PersistableEnvelope,
+        DateTolerantPayload, CapabilityRequiringPayload {
+    private static final long TOLERANCE = TimeUnit.DAYS.toMillis(1);
+
+    private final boolean signedByArbitrator;
+    private final byte[] witnessHash;
+    private final byte[] signature;
+    private final byte[] signerPubKey;
+    private final byte[] witnessOwnerPubKey;
+    private final long date;
+    private final long tradeAmount;
+
+    transient private final byte[] hash;
+
+    public SignedWitness(boolean signedByArbitrator,
+                         byte[] witnessHash,
+                         byte[] signature,
+                         byte[] signerPubKey,
+                         byte[] witnessOwnerPubKey,
+                         long date,
+                         long tradeAmount) {
+        this.signedByArbitrator = signedByArbitrator;
+        this.witnessHash = witnessHash;
+        this.signature = signature;
+        this.signerPubKey = signerPubKey;
+        this.witnessOwnerPubKey = witnessOwnerPubKey;
+        this.date = date;
+        this.tradeAmount = tradeAmount;
+
+        // The hash is only using the data which do not change in repeated trades between same users (no date or amount).
+        // We only want to store the first and oldest one and will ignore others. That will help also to protect privacy
+        // so that the total number of trades is not revealed. We use putIfAbsent when we store the data so first
+        // object will win. We consider one signed trade with one peer enough and do not consider repeated trades with
+        // same peer to add more security as if that one would be colluding it would be not detected anyway. The total
+        // number of signed trades with different peers is still available and can be considered more valuable data for
+        // security.
+        byte[] data = Utilities.concatenateByteArrays(witnessHash, signature);
+        data = Utilities.concatenateByteArrays(data, signerPubKey);
+        hash = Hash.getSha256Ripemd160hash(data);
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // PROTO BUFFER
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public protobuf.PersistableNetworkPayload toProtoMessage() {
+        final protobuf.SignedWitness.Builder builder = protobuf.SignedWitness.newBuilder()
+                .setSignedByArbitrator(signedByArbitrator)
+                .setWitnessHash(ByteString.copyFrom(witnessHash))
+                .setSignature(ByteString.copyFrom(signature))
+                .setSignerPubKey(ByteString.copyFrom(signerPubKey))
+                .setWitnessOwnerPubKey(ByteString.copyFrom(witnessOwnerPubKey))
+                .setDate(date)
+                .setTradeAmount(tradeAmount);
+        return protobuf.PersistableNetworkPayload.newBuilder().setSignedWitness(builder).build();
+    }
+
+    public protobuf.SignedWitness toProtoSignedWitness() {
+        return toProtoMessage().getSignedWitness();
+    }
+
+    public static SignedWitness fromProto(protobuf.SignedWitness proto) {
+        return new SignedWitness(proto.getSignedByArbitrator(),
+                proto.getWitnessHash().toByteArray(),
+                proto.getSignature().toByteArray(),
+                proto.getSignerPubKey().toByteArray(),
+                proto.getWitnessOwnerPubKey().toByteArray(),
+                proto.getDate(),
+                proto.getTradeAmount());
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // API
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public boolean isDateInTolerance() {
+        // We don't allow older or newer then 1 day.
+        // Preventing forward dating is also important to protect against a sophisticated attack
+        return Math.abs(new Date().getTime() - date) <= TOLERANCE;
+    }
+
+    @Override
+    public boolean verifyHashSize() {
+        return hash.length == 20;
+    }
+
+    // Pre 1.0.1 version don't know the new message type and throw an error which leads to disconnecting the peer.
+    @Override
+    public Capabilities getRequiredCapabilities() {
+        return new Capabilities(Capability.SIGNED_ACCOUNT_AGE_WITNESS);
+    }
+
+    @Override
+    public byte[] getHash() {
+        return hash;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // Getters
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    public P2PDataStorage.ByteArray getHashAsByteArray() {
+        return new P2PDataStorage.ByteArray(hash);
+    }
+
+    @Override
+    public String toString() {
+        return "SignedWitness{" +
+                ",\n     signedByArbitrator=" + signedByArbitrator +
+                ",\n     witnessHash=" + Utilities.bytesAsHexString(witnessHash) +
+                ",\n     signature=" + Utilities.bytesAsHexString(signature) +
+                ",\n     signerPubKey=" + Utilities.bytesAsHexString(signerPubKey) +
+                ",\n     witnessOwnerPubKey=" + Utilities.bytesAsHexString(witnessOwnerPubKey) +
+                ",\n     date=" + date +
+                ",\n     tradeAmount=" + Coin.valueOf(tradeAmount).toFriendlyString() +
+                ",\n     hash=" + Utilities.bytesAsHexString(hash) +
+                "\n}";
+    }
+}

core/src/main/java/bisq/core/account/sign/SignedWitnessStore.java
@@ -15,7 +15,8 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.core.payment;
+package bisq.core.account.sign;
+
 
 import bisq.network.p2p.storage.P2PDataStorage;
 import bisq.network.p2p.storage.payload.PersistableNetworkPayload;
@@ -35,15 +36,15 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * We store only the payload in the PB file to save disc space. The hash of the payload can be created anyway and
- * is only used as key in the map. So we have a hybrid data structure which is represented as list in the protobuffer
+ * is only used as key in the map. So we have a hybrid data structure which is represented as list in the protobuf
  * definition and provide a hashMap for the domain access.
  */
 @Slf4j
-public class AccountAgeWitnessStore implements PersistableEnvelope {
+public class SignedWitnessStore implements PersistableEnvelope {
     @Getter
     private Map<P2PDataStorage.ByteArray, PersistableNetworkPayload> map = new ConcurrentHashMap<>();
 
-    AccountAgeWitnessStore() {
+    SignedWitnessStore() {
     }
 
 
@@ -51,28 +52,28 @@ public class AccountAgeWitnessStore implements PersistableEnvelope {
     // PROTO BUFFER
     ///////////////////////////////////////////////////////////////////////////////////////////
 
-    private AccountAgeWitnessStore(List<AccountAgeWitness> list) {
+    private SignedWitnessStore(List<SignedWitness> list) {
         list.forEach(item -> map.put(new P2PDataStorage.ByteArray(item.getHash()), item));
     }
 
     public Message toProtoMessage() {
         return protobuf.PersistableEnvelope.newBuilder()
-                .setAccountAgeWitnessStore(getBuilder())
+                .setSignedWitnessStore(getBuilder())
                 .build();
     }
 
-    private protobuf.AccountAgeWitnessStore.Builder getBuilder() {
-        final List<protobuf.AccountAgeWitness> protoList = map.values().stream()
-                .map(payload -> (AccountAgeWitness) payload)
-                .map(AccountAgeWitness::toProtoAccountAgeWitness)
+    private protobuf.SignedWitnessStore.Builder getBuilder() {
+        final List<protobuf.SignedWitness> protoList = map.values().stream()
+                .map(payload -> (SignedWitness) payload)
+                .map(SignedWitness::toProtoSignedWitness)
                 .collect(Collectors.toList());
-        return protobuf.AccountAgeWitnessStore.newBuilder().addAllItems(protoList);
+        return protobuf.SignedWitnessStore.newBuilder().addAllItems(protoList);
     }
 
-    public static PersistableEnvelope fromProto(protobuf.AccountAgeWitnessStore proto) {
-        List<AccountAgeWitness> list = proto.getItemsList().stream()
-                .map(AccountAgeWitness::fromProto).collect(Collectors.toList());
-        return new AccountAgeWitnessStore(list);
+    public static PersistableEnvelope fromProto(protobuf.SignedWitnessStore proto) {
+        List<SignedWitness> list = proto.getItemsList().stream()
+                .map(SignedWitness::fromProto).collect(Collectors.toList());
+        return new SignedWitnessStore(list);
     }
 
     public boolean containsKey(P2PDataStorage.ByteArray hash) {

core/src/main/java/bisq/core/account/witness/AccountAgeWitnessStore.java
@@ -1,0 +1,81 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.core.account.witness;
+
+import bisq.network.p2p.storage.P2PDataStorage;
+import bisq.network.p2p.storage.payload.PersistableNetworkPayload;
+
+import bisq.common.proto.persistable.PersistableEnvelope;
+
+import com.google.protobuf.Message;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+
+/**
+ * We store only the payload in the PB file to save disc space. The hash of the payload can be created anyway and
+ * is only used as key in the map. So we have a hybrid data structure which is represented as list in the protobuffer
+ * definition and provide a hashMap for the domain access.
+ */
+@Slf4j
+public class AccountAgeWitnessStore implements PersistableEnvelope {
+    @Getter
+    private Map<P2PDataStorage.ByteArray, PersistableNetworkPayload> map = new ConcurrentHashMap<>();
+
+    AccountAgeWitnessStore() {
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // PROTO BUFFER
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    private AccountAgeWitnessStore(List<AccountAgeWitness> list) {
+        list.forEach(item -> map.put(new P2PDataStorage.ByteArray(item.getHash()), item));
+    }
+
+    public Message toProtoMessage() {
+        return protobuf.PersistableEnvelope.newBuilder()
+                .setAccountAgeWitnessStore(getBuilder())
+                .build();
+    }
+
+    private protobuf.AccountAgeWitnessStore.Builder getBuilder() {
+        final List<protobuf.AccountAgeWitness> protoList = map.values().stream()
+                .map(payload -> (AccountAgeWitness) payload)
+                .map(AccountAgeWitness::toProtoAccountAgeWitness)
+                .collect(Collectors.toList());
+        return protobuf.AccountAgeWitnessStore.newBuilder().addAllItems(protoList);
+    }
+
+    public static PersistableEnvelope fromProto(protobuf.AccountAgeWitnessStore proto) {
+        List<AccountAgeWitness> list = proto.getItemsList().stream()
+                .map(AccountAgeWitness::fromProto).collect(Collectors.toList());
+        return new AccountAgeWitnessStore(list);
+    }
+
+    public boolean containsKey(P2PDataStorage.ByteArray hash) {
+        return map.containsKey(hash);
+    }
+}