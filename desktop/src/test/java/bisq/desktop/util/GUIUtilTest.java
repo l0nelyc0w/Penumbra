l0nelyc0w/Penumbra

commit 77eaca3866566f1114962c2ad6ceb867ff47003b

Reduce number of decimals shown for monetary fiat prices to 2 (#3926)
* Remove decimals for displayed fiat volume amounts

* Reset rounded for privacy info when switching between payment methods

Co-authored-by: Christoph Atteneder <christoph.atteneder@gmail.com>

desktop/src/main/java/bisq/desktop/util/DisplayUtils.java
@@ -32,7 +32,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DisplayUtils {
     private final static int scale = 3;
-    private static final MonetaryFormat fiatVolumeFormat = new MonetaryFormat().shift(0).minDecimals(2).repeatOptionalDecimals(0, 0);
+    private static final MonetaryFormat fiatVolumeFormat = new MonetaryFormat().shift(0).minDecimals(0).repeatOptionalDecimals(0, 0);
 
     public static String formatDateTime(Date date) {
         return FormattingUtils.formatDateTime(date, true);

desktop/src/main/java/bisq/desktop/components/InfoInputTextField.java
@@ -115,6 +115,11 @@ public class InfoInputTextField extends AnchorPane {
         setActionHandlers(node);
     }
 
+    public void hideInfoContent() {
+        currentIcon = null;
+        hideIcons();
+    }
+
     public void setIconsRightAligned() {
         AnchorPane.clearConstraints(infoIcon);
         AnchorPane.clearConstraints(warningIcon);

desktop/src/main/java/bisq/desktop/main/offer/MutableOfferView.java
@@ -763,6 +763,8 @@ public abstract class MutableOfferView<M extends MutableOfferViewModel<?>> exten
         volumeListener = (observable, oldValue, newValue) -> {
             if (!newValue.equals("") && CurrencyUtil.isFiatCurrency(model.tradeCurrencyCode.get())) {
                 volumeInfoInputTextField.setContentForPrivacyPopOver(createPopoverLabel(Res.get("offerbook.info.roundedFiatVolume")));
+            } else {
+                volumeInfoInputTextField.hideInfoContent();
             }
         };
 

desktop/src/test/java/bisq/desktop/main/market/offerbook/OfferBookChartViewModelTest.java
@@ -121,11 +121,11 @@ public class OfferBookChartViewModelTest {
 
         final OfferBookChartViewModel model = new OfferBookChartViewModel(offerBook, empty, service, null, null);
         model.activate();
-        assertEquals(4, model.maxPlacesForBuyVolume.intValue()); //0.01
+        assertEquals(1, model.maxPlacesForBuyVolume.intValue()); //0
         offerBookListItems.addAll(make(btcBuyItem.but(with(OfferBookListItemMaker.amount, 100000000L))));
-        assertEquals(5, model.maxPlacesForBuyVolume.intValue()); //10.00
+        assertEquals(2, model.maxPlacesForBuyVolume.intValue()); //10
         offerBookListItems.addAll(make(btcBuyItem.but(with(OfferBookListItemMaker.amount, 22128600000L))));
-        assertEquals(7, model.maxPlacesForBuyVolume.intValue()); //2212.86
+        assertEquals(4, model.maxPlacesForBuyVolume.intValue()); //2213
     }
 
     @Test
@@ -199,10 +199,10 @@ public class OfferBookChartViewModelTest {
 
         final OfferBookChartViewModel model = new OfferBookChartViewModel(offerBook, empty, service, null, null);
         model.activate();
-        assertEquals(4, model.maxPlacesForSellVolume.intValue()); //0.01
+        assertEquals(1, model.maxPlacesForSellVolume.intValue()); //0
         offerBookListItems.addAll(make(btcSellItem.but(with(OfferBookListItemMaker.amount, 100000000L))));
-        assertEquals(5, model.maxPlacesForSellVolume.intValue()); //10.00
+        assertEquals(2, model.maxPlacesForSellVolume.intValue()); //10
         offerBookListItems.addAll(make(btcSellItem.but(with(OfferBookListItemMaker.amount, 22128600000L))));
-        assertEquals(7, model.maxPlacesForSellVolume.intValue()); //2212.86
+        assertEquals(4, model.maxPlacesForSellVolume.intValue()); //2213
     }
 }

desktop/src/test/java/bisq/desktop/main/offer/offerbook/OfferBookViewModelTest.java
@@ -298,9 +298,9 @@ public class OfferBookViewModelTest {
                 null, null, null, null, coinFormatter, new BsqFormatter());
         model.activate();
 
-        assertEquals(8, model.maxPlacesForVolume.intValue());
+        assertEquals(5, model.maxPlacesForVolume.intValue());
         offerBookListItems.addAll(make(btcBuyItem.but(with(amount, 2000000000L))));
-        assertEquals(10, model.maxPlacesForVolume.intValue());
+        assertEquals(7, model.maxPlacesForVolume.intValue());
     }
 
     @Test
@@ -316,12 +316,12 @@ public class OfferBookViewModelTest {
                 null, null, null, null, coinFormatter, new BsqFormatter());
         model.activate();
 
-        assertEquals(15, model.maxPlacesForVolume.intValue());
+        assertEquals(9, model.maxPlacesForVolume.intValue());
         offerBookListItems.addAll(make(btcItemWithRange.but(with(amount, 2000000000L))));
-        assertEquals(17, model.maxPlacesForVolume.intValue());
+        assertEquals(11, model.maxPlacesForVolume.intValue());
         offerBookListItems.addAll(make(btcItemWithRange.but(with(minAmount, 30000000000L),
                 with(amount, 30000000000L))));
-        assertEquals(25, model.maxPlacesForVolume.intValue());
+        assertEquals(19, model.maxPlacesForVolume.intValue());
     }
 
     @Test

desktop/src/test/java/bisq/desktop/util/DisplayUtilsTest.java
@@ -49,9 +49,9 @@ public class DisplayUtilsTest {
 
     @Test
     public void testFormatVolume() {
-        assertEquals("1.00", DisplayUtils.formatVolume(make(btcUsdOffer), true, 4));
-        assertEquals("100.00", DisplayUtils.formatVolume(make(usdVolume)));
-        assertEquals("1774.62", DisplayUtils.formatVolume(make(usdVolume.but(with(volumeString, "1774.62")))));
+        assertEquals("1", DisplayUtils.formatVolume(make(btcUsdOffer), true, 4));
+        assertEquals("100", DisplayUtils.formatVolume(make(usdVolume)));
+        assertEquals("1775", DisplayUtils.formatVolume(make(usdVolume.but(with(volumeString, "1774.62")))));
     }
 
     @Test

commit 106358c824d10047351902b2162696c09b96e451

Fix incorrect rounding of BSQ dollar price to whole number
Add a new method to DisplayUtils to restore the old rounding behaviour
of formatVolumeWithCode whenever a fractional volume is required. This
fixes a regression caused by #3926 to remove unnecessarily displayed
decimals for fiat volumes - it appears that in every case but the avg.
dollar price on the BSQ dashboard a whole number should be shown.

Also add a relevant test.

desktop/src/main/java/bisq/desktop/util/DisplayUtils.java
@@ -116,6 +116,10 @@ public class DisplayUtils {
         return formatVolume(volume, FIAT_VOLUME_FORMAT, true);
     }
 
+    static String formatAverageVolumeWithCode(Volume volume) {
+        return formatVolume(volume, FIAT_VOLUME_FORMAT.minDecimals(2), true);
+    }
+
     public static String formatVolumeLabel(String currencyCode) {
         return formatVolumeLabel(currencyCode, "");
     }

desktop/src/main/java/bisq/desktop/util/GUIUtil.java
@@ -949,7 +949,7 @@ public class GUIUtil {
         showBsqFeeInfoPopup(fee, miningFee, null, txSize, bsqFormatter, btcFormatter, type, actionHandler);
     }
 
-    public static void setFitToRowsForTableView(TableView tableView,
+    public static void setFitToRowsForTableView(TableView<?> tableView,
                                                 int rowHeight,
                                                 int headerHeight,
                                                 int minNumRows,
@@ -1100,7 +1100,7 @@ public class GUIUtil {
         Volume bsqAmountAsVolume = Volume.parse(bsqAmountAsString, "BSQ");
         Coin requiredBtc = bsqPrice.getAmountByVolume(bsqAmountAsVolume);
         Volume volumeByAmount = usdPrice.getVolumeByAmount(requiredBtc);
-        return DisplayUtils.formatVolumeWithCode(volumeByAmount);
+        return DisplayUtils.formatAverageVolumeWithCode(volumeByAmount);
     }
 
     public static MaterialDesignIcon getIconForSignState(AccountAgeWitnessService.SignState state) {

desktop/src/test/java/bisq/desktop/util/GUIUtilTest.java
@@ -22,8 +22,12 @@ import bisq.desktop.util.validation.RegexValidator;
 import bisq.core.locale.GlobalSettings;
 import bisq.core.locale.Res;
 import bisq.core.locale.TradeCurrency;
+import bisq.core.monetary.Price;
+import bisq.core.provider.price.MarketPrice;
+import bisq.core.provider.price.PriceFeedService;
 import bisq.core.user.DontShowAgainLookup;
 import bisq.core.user.Preferences;
+import bisq.core.util.coin.BsqFormatter;
 
 import org.bitcoinj.core.Coin;
 import org.bitcoinj.core.CoinMaker;
@@ -51,7 +55,6 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
 @Ignore
 public class GUIUtilTest {
 
@@ -65,7 +68,7 @@ public class GUIUtilTest {
 
     @Test
     public void testTradeCurrencyConverter() {
-        Map<String, Integer> offerCounts = new HashMap<String, Integer>() {{
+        Map<String, Integer> offerCounts = new HashMap<>() {{
             put("BTC", 11);
             put("EUR", 10);
         }};
@@ -80,7 +83,7 @@ public class GUIUtilTest {
     }
 
     @Test
-    public void testOpenURLWithCampaignParameters() throws Exception {
+    public void testOpenURLWithCampaignParameters() {
         Preferences preferences = mock(Preferences.class);
         DontShowAgainLookup.setPreferences(preferences);
         GUIUtil.setPreferences(preferences);
@@ -101,7 +104,7 @@ public class GUIUtilTest {
     }
 
     @Test
-    public void testOpenURLWithoutCampaignParameters() throws Exception {
+    public void testOpenURLWithoutCampaignParameters() {
         Preferences preferences = mock(Preferences.class);
         DontShowAgainLookup.setPreferences(preferences);
         GUIUtil.setPreferences(preferences);
@@ -141,6 +144,18 @@ public class GUIUtilTest {
 
         assertFalse(regexValidator.validate("12.34.56.788").isValid);
         assertFalse(regexValidator.validate("12.34.56.78:").isValid);
+    }
+
+    @Test
+    public void testGetBsqInUsd() {
+        PriceFeedService priceFeedService = mock(PriceFeedService.class);
+        when(priceFeedService.getMarketPrice("USD"))
+                .thenReturn(new MarketPrice("USD", 12345.6789, 0, true));
+
+        Coin oneBsq = Coin.valueOf(100);
+        Price avgPrice = Price.valueOf("BSQ", 10000);
+
+        assertEquals("1.23 USD", GUIUtil.getBsqInUsd(avgPrice, oneBsq, priceFeedService, new BsqFormatter()));
     }
 
     @Test