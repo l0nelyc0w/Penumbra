bisq-network/bisq

bitcoinj 0.15: Replace deprecated Transaction.getHash() with Transaction.getTxId().

core/src/main/java/bisq/core/trade/Trade.java
@@ -547,7 +547,7 @@ public abstract class Trade implements Tradable, Model {
     // The deserialized tx has not actual confidence data, so we need to get the fresh one from the wallet.
     public void updateDepositTxFromWallet() {
         if (getDepositTx() != null)
-            setDepositTx(processModel.getTradeWalletService().getWalletTx(getDepositTx().getHash()));
+            setDepositTx(processModel.getTradeWalletService().getWalletTx(getDepositTx().getTxId()));
     }
 
     @SuppressWarnings("NullableProblems")

l0nelyc0w/Penumbra

core/src/main/java/bisq/core/trade/Trade.java
@@ -695,7 +695,7 @@ public abstract class Trade implements Tradable, Model {
     // The deserialized tx has not actual confidence data, so we need to get the fresh one from the wallet.
     void updateDepositTxFromWallet() {
         if (getDepositTx() != null)
-            applyDepositTx(processModel.getTradeWalletService().getWalletTx(getDepositTx().getHash()));
+            applyDepositTx(processModel.getTradeWalletService().getWalletTx(getDepositTx().getTxId()));
     }
 
     public void applyDepositTx(Transaction tx) {