bisq-network/bisq

bitcoinj 0.15: Use LegacyAddress.fromBase58() instead of Address.fromBase58().

core/src/main/java/bisq/core/btc/wallet/TradeWalletService.java
@@ -36,6 +36,7 @@ import org.bitcoinj.core.Coin;
 import org.bitcoinj.core.Context;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.InsufficientMoneyException;
+import org.bitcoinj.core.LegacyAddress;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.Sha256Hash;
 import org.bitcoinj.core.Transaction;
@@ -191,7 +192,7 @@ public class TradeWalletService {
         Transaction tradingFeeTx = new Transaction(params);
         SendRequest sendRequest = null;
         try {
-            tradingFeeTx.addOutput(tradingFee, Address.fromBase58(params, feeReceiverAddresses));
+            tradingFeeTx.addOutput(tradingFee, LegacyAddress.fromBase58(params, feeReceiverAddresses));
             // the reserved amount we need for the trade we send to our trade reservedForTradeAddress
             tradingFeeTx.addOutput(reservedFundsForOffer, reservedForTradeAddress);
 
@@ -514,7 +515,7 @@ public class TradeWalletService {
         TransactionOutput takerTransactionOutput = null;
         if (takerChangeOutputValue > 0 && takerChangeAddressString != null)
             takerTransactionOutput = new TransactionOutput(params, preparedDepositTx, Coin.valueOf(takerChangeOutputValue),
-                    Address.fromBase58(params, takerChangeAddressString));
+                    LegacyAddress.fromBase58(params, takerChangeAddressString));
 
         if (makerIsBuyer) {
             // Add optional buyer outputs
@@ -839,9 +840,9 @@ public class TradeWalletService {
         Transaction preparedPayoutTx = new Transaction(params);
         preparedPayoutTx.addInput(p2SHMultiSigOutput);
         if (buyerPayoutAmount.isGreaterThan(Coin.ZERO))
-            preparedPayoutTx.addOutput(buyerPayoutAmount, Address.fromBase58(params, buyerAddressString));
+            preparedPayoutTx.addOutput(buyerPayoutAmount, LegacyAddress.fromBase58(params, buyerAddressString));
         if (sellerPayoutAmount.isGreaterThan(Coin.ZERO))
-            preparedPayoutTx.addOutput(sellerPayoutAmount, Address.fromBase58(params, sellerAddressString));
+            preparedPayoutTx.addOutput(sellerPayoutAmount, LegacyAddress.fromBase58(params, sellerAddressString));
 
         // take care of sorting!
         Script redeemScript = getMultiSigRedeemScript(buyerPubKey, sellerPubKey, arbitratorPubKey);
@@ -908,9 +909,9 @@ public class TradeWalletService {
         Transaction payoutTx = new Transaction(params);
         payoutTx.addInput(p2SHMultiSigOutput);
         if (buyerPayoutAmount.isGreaterThan(Coin.ZERO))
-            payoutTx.addOutput(buyerPayoutAmount, Address.fromBase58(params, buyerAddressString));
+            payoutTx.addOutput(buyerPayoutAmount, LegacyAddress.fromBase58(params, buyerAddressString));
         if (sellerPayoutAmount.isGreaterThan(Coin.ZERO))
-            payoutTx.addOutput(sellerPayoutAmount, Address.fromBase58(params, sellerAddressString));
+            payoutTx.addOutput(sellerPayoutAmount, LegacyAddress.fromBase58(params, sellerAddressString));
 
         // take care of sorting!
         Script redeemScript = getMultiSigRedeemScript(buyerPubKey, sellerPubKey, arbitratorPubKey);
@@ -993,11 +994,11 @@ public class TradeWalletService {
         payoutTx.addInput(new TransactionInput(params, depositTx, p2SHMultiSigOutputScript.getProgram(), new TransactionOutPoint(params, 0, spendTxHash), msOutput));
 
         if (buyerPayoutAmount.isGreaterThan(Coin.ZERO))
-            payoutTx.addOutput(buyerPayoutAmount, Address.fromBase58(params, buyerAddressString));
+            payoutTx.addOutput(buyerPayoutAmount, LegacyAddress.fromBase58(params, buyerAddressString));
         if (sellerPayoutAmount.isGreaterThan(Coin.ZERO))
-            payoutTx.addOutput(sellerPayoutAmount, Address.fromBase58(params, sellerAddressString));
+            payoutTx.addOutput(sellerPayoutAmount, LegacyAddress.fromBase58(params, sellerAddressString));
         if (arbitratorPayoutAmount.isGreaterThan(Coin.ZERO))
-            payoutTx.addOutput(arbitratorPayoutAmount, Address.fromBase58(params, arbitratorAddressString));
+            payoutTx.addOutput(arbitratorPayoutAmount, LegacyAddress.fromBase58(params, arbitratorAddressString));
 
         // take care of sorting!
         Script redeemScript = getMultiSigRedeemScript(buyerPubKey, sellerPubKey, arbitratorPubKey);
@@ -1166,8 +1167,8 @@ public class TradeWalletService {
         TransactionOutput p2SHMultiSigOutput = depositTx.getOutput(0);
         Transaction transaction = new Transaction(params);
         transaction.addInput(p2SHMultiSigOutput);
-        transaction.addOutput(buyerPayoutAmount, Address.fromBase58(params, buyerAddressString));
-        transaction.addOutput(sellerPayoutAmount, Address.fromBase58(params, sellerAddressString));
+        transaction.addOutput(buyerPayoutAmount, LegacyAddress.fromBase58(params, buyerAddressString));
+        transaction.addOutput(sellerPayoutAmount, LegacyAddress.fromBase58(params, sellerAddressString));
         return transaction;
     }
 

core/src/main/java/bisq/core/dao/state/model/governance/ReimbursementProposal.java
@@ -31,6 +31,7 @@ import io.bisq.generated.protobuffer.PB;
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.AddressFormatException;
 import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.LegacyAddress;
 
 import org.springframework.util.CollectionUtils;
 
@@ -123,7 +124,7 @@ public final class ReimbursementProposal extends Proposal implements IssuancePro
     public Address getAddress() throws AddressFormatException {
         // Remove leading 'B'
         String underlyingBtcAddress = bsqAddress.substring(1, bsqAddress.length());
-        return Address.fromBase58(BisqEnvironment.getParameters(), underlyingBtcAddress);
+        return LegacyAddress.fromBase58(BisqEnvironment.getParameters(), underlyingBtcAddress);
     }
 
 

core/src/main/java/bisq/core/util/BsqFormatter.java
@@ -32,6 +32,7 @@ import bisq.common.util.MathUtils;
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.AddressFormatException;
 import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.LegacyAddress;
 import org.bitcoinj.utils.MonetaryFormat;
 
 import javax.inject.Inject;
@@ -101,7 +102,7 @@ public class BsqFormatter extends BSFormatter {
             encoded = encoded.substring(prefix.length(), encoded.length());
 
         try {
-            return Address.fromBase58(BisqEnvironment.getParameters(), encoded);
+            return LegacyAddress.fromBase58(BisqEnvironment.getParameters(), encoded);
         } catch (AddressFormatException e) {
             throw new RuntimeException(e);
         }

core/src/main/java/bisq/core/util/validation/BtcAddressValidator.java
@@ -22,6 +22,7 @@ import bisq.core.locale.Res;
 
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.AddressFormatException;
+import org.bitcoinj.core.LegacyAddress;
 
 import javax.inject.Inject;
 
@@ -43,7 +44,7 @@ public final class BtcAddressValidator extends InputValidator {
 
     private ValidationResult validateBtcAddress(String input) {
         try {
-            Address.fromBase58(BisqEnvironment.getParameters(), input);
+            LegacyAddress.fromBase58(BisqEnvironment.getParameters(), input);
             return new ValidationResult(true);
         } catch (AddressFormatException e) {
             return new ValidationResult(false, Res.get("validation.btc.invalidFormat"));

desktop/src/main/java/bisq/desktop/util/GUIUtil.java
@@ -56,6 +56,7 @@ import bisq.common.util.Utilities;
 
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.LegacyAddress;
 import org.bitcoinj.core.TransactionConfidence;
 import org.bitcoinj.uri.BitcoinURI;
 import org.bitcoinj.wallet.DeterministicSeed;
@@ -729,7 +730,7 @@ public class GUIUtil {
 
     public static String getBitcoinURI(String address, Coin amount, String label) {
         return address != null ?
-                BitcoinURI.convertToBitcoinURI(Address.fromBase58(BisqEnvironment.getParameters(),
+                BitcoinURI.convertToBitcoinURI(LegacyAddress.fromBase58(BisqEnvironment.getParameters(),
                         address), amount, label, null) :
                 "";
     }

l0nelyc0w/Penumbra

core/src/main/java/bisq/core/btc/wallet/TradeWalletService.java
@@ -36,6 +36,7 @@ import org.bitcoinj.core.AddressFormatException;
 import org.bitcoinj.core.Coin;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.InsufficientMoneyException;
+import org.bitcoinj.core.LegacyAddress;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.Sha256Hash;
 import org.bitcoinj.core.Transaction;
@@ -151,7 +152,7 @@ public class TradeWalletService {
         Transaction tradingFeeTx = new Transaction(params);
         SendRequest sendRequest = null;
         try {
-            tradingFeeTx.addOutput(tradingFee, Address.fromBase58(params, feeReceiverAddress));
+            tradingFeeTx.addOutput(tradingFee, LegacyAddress.fromBase58(params, feeReceiverAddress));
             // the reserved amount we need for the trade we send to our trade reservedForTradeAddress
             tradingFeeTx.addOutput(reservedFundsForOffer, reservedForTradeAddress);
 
@@ -515,7 +516,7 @@ public class TradeWalletService {
         TransactionOutput takerTransactionOutput = null;
         if (takerChangeOutputValue > 0 && takerChangeAddressString != null) {
             takerTransactionOutput = new TransactionOutput(params, preparedDepositTx, Coin.valueOf(takerChangeOutputValue),
-                    Address.fromBase58(params, takerChangeAddressString));
+                    LegacyAddress.fromBase58(params, takerChangeAddressString));
         }
 
         if (makerIsBuyer) {
@@ -685,7 +686,7 @@ public class TradeWalletService {
         delayedPayoutTx.addInput(p2SHMultiSigOutput);
         applyLockTime(lockTime, delayedPayoutTx);
         Coin outputAmount = p2SHMultiSigOutput.getValue().subtract(minerFee);
-        delayedPayoutTx.addOutput(outputAmount, Address.fromBase58(params, donationAddressString));
+        delayedPayoutTx.addOutput(outputAmount, LegacyAddress.fromBase58(params, donationAddressString));
         WalletService.printTx("Unsigned delayedPayoutTx ToDonationAddress", delayedPayoutTx);
         WalletService.verifyTransaction(delayedPayoutTx);
         return delayedPayoutTx;
@@ -937,10 +938,10 @@ public class TradeWalletService {
         Transaction payoutTx = new Transaction(params);
         payoutTx.addInput(p2SHMultiSigOutput);
         if (buyerPayoutAmount.isPositive()) {
-            payoutTx.addOutput(buyerPayoutAmount, Address.fromBase58(params, buyerAddressString));
+            payoutTx.addOutput(buyerPayoutAmount, LegacyAddress.fromBase58(params, buyerAddressString));
         }
         if (sellerPayoutAmount.isPositive()) {
-            payoutTx.addOutput(sellerPayoutAmount, Address.fromBase58(params, sellerAddressString));
+            payoutTx.addOutput(sellerPayoutAmount, LegacyAddress.fromBase58(params, sellerAddressString));
         }
 
         // take care of sorting!
@@ -1000,10 +1001,10 @@ public class TradeWalletService {
         payoutTx.addInput(new TransactionInput(params, depositTx, p2SHMultiSigOutputScript.getProgram(), new TransactionOutPoint(params, 0, spendTxHash), msOutput));
 
         if (buyerPayoutAmount.isPositive()) {
-            payoutTx.addOutput(buyerPayoutAmount, Address.fromBase58(params, buyerAddressString));
+            payoutTx.addOutput(buyerPayoutAmount, LegacyAddress.fromBase58(params, buyerAddressString));
         }
         if (sellerPayoutAmount.isPositive()) {
-            payoutTx.addOutput(sellerPayoutAmount, Address.fromBase58(params, sellerAddressString));
+            payoutTx.addOutput(sellerPayoutAmount, LegacyAddress.fromBase58(params, sellerAddressString));
         }
 
         // take care of sorting!
@@ -1145,10 +1146,10 @@ public class TradeWalletService {
         Transaction transaction = new Transaction(params);
         transaction.addInput(p2SHMultiSigOutput);
         if (buyerPayoutAmount.isPositive()) {
-            transaction.addOutput(buyerPayoutAmount, Address.fromBase58(params, buyerAddressString));
+            transaction.addOutput(buyerPayoutAmount, LegacyAddress.fromBase58(params, buyerAddressString));
         }
         if (sellerPayoutAmount.isPositive()) {
-            transaction.addOutput(sellerPayoutAmount, Address.fromBase58(params, sellerAddressString));
+            transaction.addOutput(sellerPayoutAmount, LegacyAddress.fromBase58(params, sellerAddressString));
         }
         checkArgument(transaction.getOutputs().size() >= 1, "We need at least one output.");
         return transaction;

core/src/main/java/bisq/core/dao/state/model/governance/ReimbursementProposal.java
@@ -30,6 +30,7 @@ import bisq.common.util.CollectionUtils;
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.AddressFormatException;
 import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.LegacyAddress;
 
 import java.util.Date;
 import java.util.Map;
@@ -120,7 +121,7 @@ public final class ReimbursementProposal extends Proposal implements IssuancePro
     public Address getAddress() throws AddressFormatException {
         // Remove leading 'B'
         String underlyingBtcAddress = bsqAddress.substring(1, bsqAddress.length());
-        return Address.fromBase58(Config.baseCurrencyNetworkParameters(), underlyingBtcAddress);
+        return LegacyAddress.fromBase58(Config.baseCurrencyNetworkParameters(), underlyingBtcAddress);
     }
 
 

core/src/main/java/bisq/core/util/coin/BsqFormatter.java
@@ -34,6 +34,7 @@ import bisq.common.util.MathUtils;
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.AddressFormatException;
 import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.LegacyAddress;
 import org.bitcoinj.utils.MonetaryFormat;
 
 import javax.inject.Inject;
@@ -110,7 +111,7 @@ public class BsqFormatter implements CoinFormatter {
             encoded = encoded.substring(prefix.length(), encoded.length());
 
         try {
-            return Address.fromBase58(Config.baseCurrencyNetworkParameters(), encoded);
+            return LegacyAddress.fromBase58(Config.baseCurrencyNetworkParameters(), encoded);
         } catch (AddressFormatException e) {
             throw new RuntimeException(e);
         }

core/src/main/java/bisq/core/util/validation/BtcAddressValidator.java
@@ -23,6 +23,7 @@ import bisq.common.config.Config;
 
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.AddressFormatException;
+import org.bitcoinj.core.LegacyAddress;
 
 import javax.inject.Inject;
 
@@ -44,7 +45,7 @@ public final class BtcAddressValidator extends InputValidator {
 
     private ValidationResult validateBtcAddress(String input) {
         try {
-            Address.fromBase58(Config.baseCurrencyNetworkParameters(), input);
+            LegacyAddress.fromBase58(Config.baseCurrencyNetworkParameters(), input);
             return new ValidationResult(true);
         } catch (AddressFormatException e) {
             return new ValidationResult(false, Res.get("validation.btc.invalidFormat"));

desktop/src/main/java/bisq/desktop/util/GUIUtil.java
@@ -70,6 +70,7 @@ import bisq.common.util.Utilities;
 
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.LegacyAddress;
 import org.bitcoinj.core.TransactionConfidence;
 import org.bitcoinj.uri.BitcoinURI;
 import org.bitcoinj.utils.Fiat;
@@ -718,7 +719,7 @@ public class GUIUtil {
 
     public static String getBitcoinURI(String address, Coin amount, String label) {
         return address != null ?
-                BitcoinURI.convertToBitcoinURI(Address.fromBase58(Config.baseCurrencyNetworkParameters(),
+                BitcoinURI.convertToBitcoinURI(LegacyAddress.fromBase58(Config.baseCurrencyNetworkParameters(),
                         address), amount, label, null) :
                 "";
     }